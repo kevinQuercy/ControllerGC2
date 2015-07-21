7de12ade3ddc3eaaedb8739e92d5c4e760c4280b
suzyCnam <suzy.palleau@gmail.com>
2015-07-21 23:00:09 +0200

    common XMLSocket for the 3 projects

--- src/core/SocketClientHandler.java
+++ src/core/SocketClientHandler.java
@@ -1,18 +1,9 @@
 package core;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.StringReader;
 import java.net.Socket;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.jdom2.Document;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
-import org.jdom2.output.Format;
-import org.jdom2.output.XMLOutputter;
 
 /** @file
  * 
@@ -29,55 +20,32 @@ import org.jdom2.output.XMLOutputter;
 public class SocketClientHandler extends Thread {
 	private static Logger LOGGER = Logger.getLogger(SocketClientHandler.class.getName());
 
-	Socket socket;
+	XMLSocket xmlsocket;
 	int clientNumber;
 	
 	public SocketClientHandler(Socket socket, int clientNumber) {
 		super();
-		this.socket = socket;
+		this.xmlsocket = new XMLSocket(socket);
 		this.clientNumber = clientNumber;
 		LOGGER.info("New client connected: #"+clientNumber+", "+socket);
 	}
 	
 	public void run() {
-        try {
-        	SAXBuilder sxb = new SAXBuilder();
-        	RequestHandler reqHandler = new RequestHandler(clientNumber);
-        	
-    		// process all requests from client
-        	while(true) {
-        		// get XML request (buffer lines until an empty line is found)
-        		BufferedReader buffRead = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        		StringBuilder xmlRequest = new StringBuilder();
-        		while (true)
-        		{
-        			String line = buffRead.readLine();
-        			if (line == null || line.isEmpty())
-        				break;
-        			xmlRequest.append(line);
-        		}
-        		if (xmlRequest.length() == 0)
-        			break; // no request, close socket
-        		Document request = sxb.build(new StringReader(xmlRequest.toString()));
-        		
-        		// process it
-        		Document response = reqHandler.handle(request);
-        		
-        		// send response
-        		XMLOutputter xmlOutput = new XMLOutputter(Format.getCompactFormat());
-        		xmlOutput.output(response, socket.getOutputStream());
-        		socket.getOutputStream().write('\n'); // empty line to indicate end of response
-            }
-		} catch (IOException e) {
-			LOGGER.log(Level.INFO, "Client #"+clientNumber+" disconnected", e);
-		} catch (JDOMException e) {
-			LOGGER.log(Level.SEVERE, "Client #"+clientNumber+": invalid XML", e);
-		} finally {
-			try {
-				socket.close();
-			} catch (IOException e) {
-				LOGGER.log(Level.SEVERE, "Error while closing socket", e);
-			}
-		}
+    	RequestHandler reqHandler = new RequestHandler(clientNumber);
+    	
+		// process all requests from client
+    	while(!xmlsocket.isClosed()) {
+    		// get XML request
+    		Document request = xmlsocket.read();
+    		if (request == null)
+    				break;
+    		
+    		// process it and get associated response
+    		Document response = reqHandler.handle(request);
+    		
+    		// send XML response
+    		xmlsocket.write(response);
+        }
+    	xmlsocket.close();
     }
 }
