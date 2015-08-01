7b0566a6044a2a19ea37baaed050c6aaeb930b41
suzyCnam <suzy.palleau@gmail.com>
2015-08-01 10:29:20 +0200

    remove unused graph package

--- src/data/Standstill.java
+++ src/data/Standstill.java
@@ -1,5 +1,10 @@
 package data;
 
+/** @file
+ * 
+ * Interface used by optaplanner solver
+ */
+
 import org.optaplanner.core.api.domain.entity.PlanningEntity;
 import org.optaplanner.core.api.domain.variable.InverseRelationShadowVariable;
 
