9c4b9ce3647ab54731413af2c847ce6e62b1ae10
willyCnam <willy.platzer@gmail.com>
2015-09-06 15:51:07 +0200

    Delete WSPublisher.java

--- src/webservice/WSPublisher.java
+++
@@ -1,8 +0,0 @@
-package webservice;
-import javax.xml.ws.Endpoint;
-
-public class WSPublisher {
-    public static void main(String[] args){
-    	Endpoint.publish("http://localhost:8081/WS/WSPlanification", new WSPlanificationImp());
-    }
-}
