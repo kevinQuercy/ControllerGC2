e9b4591d449a8835870223c72bd32a204f8bf31a
willyCnam <willy.platzer@gmail.com>
2015-09-07 10:34:37 +0200

    Update BDManager.java

--- src/DAOS/BDManager.java
+++ src/DAOS/BDManager.java
@@ -2,18 +2,20 @@ package DAOS;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
-  /** 
+  /**
+   *	@author WillyCnam
+   *	@version 1.0
+   */
 
-@author WillyCnam
-@version 1.0
-
+// Configuration of the database
 public class BDManager {
-	private static String url = "jdbc:mysql://localhost:3306/garbagecollector";
+    private static String url = "jdbc:mysql://localhost:3306/garbagecollector";
     //private static String url = "jdbc:mysql://192.168.10.2:3306/garbagecollector";
     private static String driver = "com.mysql.jdbc.Driver";
     private static String login = "root";
     private static String password = "";
     
+    // Creation of a connection
     public static Connection getConnexion() throws Exception{
         Class c = Class.forName(driver);
         Driver d = (Driver) c.newInstance();
